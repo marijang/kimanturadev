df540ec9c1daa63cebbd8b2cf13f44a937ce8fd0
Marijan Greguric <marijan.greguric@in2.hr>
2018-05-09 13:29:26 +0200

    Container FIx

--- wp-content/themes/kimnatura/template-parts/single/page.php
+++ wp-content/themes/kimnatura/template-parts/single/page.php
@@ -20,7 +20,7 @@ $image  = $images->get_post_image( 'full_width' );
     </h1>
     <?php the_subtitle( '<p class="section__description">', '</p>' ); ?>
   </header>
-  <div class="section__content section__content-style section__content-media-style">
+  <div class="section__content content-about-us-style">
     <?php the_content(); ?>
   </div>
   <?php require locate_template( 'template-parts/parts/google-rich-snippets.php' ); ?>
