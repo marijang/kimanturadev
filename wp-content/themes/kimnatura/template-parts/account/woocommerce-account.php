fdc350cde57f90a33146c7d19c1739444378a319
Lovro Filipović <lovro.filipovic@in2.hr>
2018-05-10 19:11:50 +0200

    responzivni navigation user menu

--- wp-content/themes/kimnatura/template-parts/account/woocommerce-account.php
+++ wp-content/themes/kimnatura/template-parts/account/woocommerce-account.php
@@ -6,14 +6,29 @@
  */
 
 ?>
-
+<script>
+	jQuery( document ).ready(function() {
+		jQuery('#settings-toggle').on('click', function(){
+			if(jQuery('#settings-menu').hasClass('is-open')){
+				jQuery('#settings-menu').removeClass('is-open');
+				jQuery('#settings-logout').removeClass('is-open');
+        		jQuery('#settings-toggle').removeClass('is-active');
+      		}
+      		else{
+				jQuery('#settings-menu').addClass('is-open');
+				jQuery('#settings-logout').addClass('is-open');
+        		jQuery('#settings-toggle').addClass('is-active');
+      		}
+		});
+	});
+</script>
 <div class="navigation-user">
         <div class="navigation-user__wrap">
 	    <?php
 		if ( is_user_logged_in() ) {
 			$current_user = wp_get_current_user();
         ?>
-				<nav class="account__navigation-user">
+				<nav id="settings-menu" class="account__navigation-user navigation-user__account-menu">
 					<ul class="navigation-user__menu">
 						<?php 
 						$endpoints = wc_get_account_menu_items();
@@ -63,8 +78,11 @@
 				
 		<div class="navigation-user__info">
 		   <span class="navigation-user__info-name"> <?php echo $current_user->user_email?></span>
+		</div>
+		<div class="navigation-user__logout" id="settings-logout">
 		   <a href="<?php echo 	esc_url( wc_logout_url( wc_get_page_permalink( 'myaccount' ) ) ); ?>" class="navigation-user__info-link" ><?php echo __('Odjavite se','b4b') ?></a>
 		</div>
+		<div class="navigation-user__settings" id="settings-toggle"><i class="material-icons">settings</i></div>
 		<?php
 		} else {
 		?>
