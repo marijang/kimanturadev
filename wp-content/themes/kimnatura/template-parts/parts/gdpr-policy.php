5c5ffee63efde4f16d98eae187c27f6c787113b9
Marijan Greguric <marijan.greguric@in2.hr>
2018-06-21 10:40:15 +0200

    gdpr

--- wp-content/themes/kimnatura/template-parts/parts/gdpr-policy.php
+++ wp-content/themes/kimnatura/template-parts/parts/gdpr-policy.php
@@ -1,18 +1,9 @@
 
-<h4>Politika privatnosti i Politika kolačića</h4>
+<p>
 
-<p><strong>Kimnatura d.o.o.</strong> posvećuje veliku važnost zaštiti osobnih podataka naših korisnika. Politika zaštite privatnosti uređuje način postupanja s informacijama koje Kimnatura d.o.o. obrađuje odnosno prikuplja prilikom posjeta našoj internet stranici. Napominjemo kako se obrada prikupljenih osobnih podataka temelji na jednoj od zakonitih osnova obrade u skladu s Općom uredbom o zaštiti podataka. Politika zaštite privatnosti obuhvaća:</p>
-
-<ul class="browser-default">
-    <li>Korištenje i otkrivanje odnosno prijenos osobnih podataka,</li>
-    <li>Politiku vezanu uz korištenje i pohranjivanje Kolačića (HTTP cookies),</li>
-    <li>pravila vezana uz Newsletter i pretplatu,</li>
-    <li>Sigurnost,</li>
-    <li>Rokove pohranjivanja,</li>
-    <li>Prava korisnika,</li>
-    <li>Automatiziranu obradu,</li>
-    <li>Veze s drugim stranicama te Obavijest o promjenama i kontakt.</li>
-</ul>
-
+<Ova web stranica koristi kolačiće (eng. cookies) zbog pružanja boljeg korisničkog iskustva i funkcionalnosti. 
+Više informacija o kolačićima možete pronaći u našoj Politici o zaštiti privatnosti. 
+Koristeći našu stranicu, slažete se s korištenjem kolačića.
+</p>
 <p>Za potpuni tekst Politike privatnosti i Politike kolačića i detaljnije informacije o svakom pojedinom području molimo Vas da koristite poveznicu niže:</p>
 <a href="/pravila-privatnosti/" class="">Pravila privatnosti</a>
