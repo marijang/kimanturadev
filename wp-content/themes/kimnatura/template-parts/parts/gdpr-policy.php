6fbde2c7b2629c515038a3ac390bd72b4765bd17
Marijan Greguric <marijan.greguric@in2.hr>
2018-06-21 11:06:56 +0200

    cookie

--- wp-content/themes/kimnatura/template-parts/parts/gdpr-policy.php
+++ wp-content/themes/kimnatura/template-parts/parts/gdpr-policy.php
@@ -1,9 +1,8 @@
 
 <p>
-
-<Ova web stranica koristi kolačiće (eng. cookies) zbog pružanja boljeg korisničkog iskustva i funkcionalnosti. 
+Ova web stranica koristi kolačiće (eng. cookies) zbog pružanja boljeg korisničkog iskustva i funkcionalnosti. 
 Više informacija o kolačićima možete pronaći u našoj Politici o zaštiti privatnosti. 
 Koristeći našu stranicu, slažete se s korištenjem kolačića.
 </p>
 <p>Za potpuni tekst Politike privatnosti i Politike kolačića i detaljnije informacije o svakom pojedinom području molimo Vas da koristite poveznicu niže:</p>
-<a href="/pravila-privatnosti/" class="">Pravila privatnosti</a>
+<a href="/pravila-privatnosti/" class="">Pravila privatnosti</a><a href="/politika-kolacica/" class="">Politika kolačića (Cookie Policy)</a>
