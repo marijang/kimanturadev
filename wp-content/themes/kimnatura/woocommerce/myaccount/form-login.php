be526d626ebc39ff56387d4860fb4e43f9981e3a
Lovro Filipović <lovro1210@gmail.com>
2018-07-26 16:01:53 +0200

    bugovi - dorade

--- wp-content/themes/kimnatura/woocommerce/myaccount/form-login.php
+++ wp-content/themes/kimnatura/woocommerce/myaccount/form-login.php
@@ -25,31 +25,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 <script>
 	jQuery( document ).ready(function() {
 		jQuery('.ct-ultimate-gdpr-consent-field').parent().css('display', 'none');
-		jQuery('.woocommerce-privacy-policy-text').parent().css('display', 'none');
+		jQuery('.woocommerce-privacy-policy-text').css('display', 'none');
 		jQuery('#username').focus();
 		jQuery('#log-toggle').on('click', function(){
 			jQuery('#reg-box').removeClass('is-active');
 			jQuery('#log-box').addClass('is-active');
 			jQuery('.ct-ultimate-gdpr-consent-field').parent().css('display', 'none');
-			jQuery('.woocommerce-privacy-policy-text').parent().css('display', 'none');
+			jQuery('.woocommerce-privacy-policy-text').css('display', 'none');
 	});
 	jQuery('#reg-toggle').on('click', function(){
 			jQuery('#log-box').removeClass('is-active');
 			jQuery('#reg-box').addClass('is-active');
 			jQuery('.ct-ultimate-gdpr-consent-field').parent().css('display', 'inline-block');
-			jQuery('.woocommerce-privacy-policy-text').parent().css('display', 'block');
+			jQuery('.woocommerce-privacy-policy-text').css('display', 'block');
 	});
 	jQuery('#log-toggle-mobile').on('click', function(){
 			jQuery('#reg-box').removeClass('is-active');
 			jQuery('#log-box').addClass('is-active');
 			jQuery('.ct-ultimate-gdpr-consent-field').parent().css('display', 'none');
-			jQuery('.woocommerce-privacy-policy-text').parent().css('display', 'none');
+			jQuery('.woocommerce-privacy-policy-text').css('display', 'none');
 	});
 	jQuery('#reg-toggle-mobile').on('click', function(){
 			jQuery('#log-box').removeClass('is-active');
 			jQuery('#reg-box').addClass('is-active');
 			jQuery('.ct-ultimate-gdpr-consent-field').parent().css('display', 'inline-block');
-			jQuery('.woocommerce-privacy-policy-text').parent().css('display', 'block');
+			jQuery('.woocommerce-privacy-policy-text').css('display', 'block');
 	});
 });
 </script>
