60b0212e5c402ee932d9d7f62b4b18607cacfc35
mZabcic <mislav.zabcic@gmail.com>
2018-08-01 14:55:19 +0200

    Dorade

--- wp-content/themes/kimnatura/woocommerce/myaccount/form-edit-account.php
+++ wp-content/themes/kimnatura/woocommerce/myaccount/form-edit-account.php
@@ -43,6 +43,13 @@ do_action( 'woocommerce_before_edit_account_form' ); ?>
 		<input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" value="<?php echo esc_attr( $user->user_email ); ?>" />
 	</p>
 
+
+	<p class="input-field woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+		<label for="account_display_name"><?php esc_html_e( 'Display name', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="<?php echo esc_attr( $user->display_name ); ?>" /> 
+	</p>
+	<?php do_action( 'woocommerce_edit_account_form' ); ?>
+
 	<p class="login__button-row">
 		<?php wp_nonce_field( 'save_account_details' ); ?>
 		<button type="submit" class="woocommerce-Button button btn btn--primary-color" name="save_account_details" value="<?php esc_attr_e( 'Save changes', 'woocommerce' ); ?>"><?php esc_html_e( 'Save changes', 'woocommerce' ); ?></button>
