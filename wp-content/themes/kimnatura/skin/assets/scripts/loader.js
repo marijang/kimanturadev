1d39cf9e0e0a9cff3f824413b522e959e9d058a9
Lovro Filipović <lovro1210@gmail.com>
2018-09-05 18:09:20 +0200

    loader

--- wp-content/themes/kimnatura/skin/assets/scripts/loader.js
+++ wp-content/themes/kimnatura/skin/assets/scripts/loader.js
@@ -1,34 +1,64 @@
 import $ from 'jquery';
 global.$ = global.jQuery = $;
-$(function() {
-        $(window).on("load", function() {
-            console.log('ulaz');
-            if (localStorage.getItem("lastRun") === null /*|| (Math.abs(new Date(localStorage.getItem("lastRun")) - new Date()) / 36e5) > 1*/) {
-                $("body").addClass("loader-open");
+
+function showLoader(){
+    if (localStorage.getItem("lastRun") === null /*|| (Math.abs(new Date(localStorage.getItem("lastRun")) - new Date()) / 36e5) > 1*/) {
+                        $("body").addClass("loader-open");
+                     
+                        $(".loader__wrap").addClass("view");
+                        $('.loader-hidden').removeClass('loader-hidden');
+                        localStorage.setItem('lastRun', new Date());
+                        setTimeout(function() {
+                        
+                            $(".loader__wrap").addClass("loader-hidden");
+                            setTimeout(function() {
+                                $("body").removeClass("loader-open");
+                               
+                            },2000)
+                         }, 2500); //1500
+                   }
+                   else{
+                   // $('.loader__wrap').addClass('loader-hidden');
+                   console.log('remove');
+                    $('.loader__wrap').remove();
+                    
+                   }
+}
+
+$(document).ready(function() {
+    showLoader();
+});
+
+// $(document).ready(function() {
+//         $(window).on("load", function() {
+//             console.log('ulaz');
+//             if (localStorage.getItem("lastRun") === null /*|| (Math.abs(new Date(localStorage.getItem("lastRun")) - new Date()) / 36e5) > 1*/) {
+//                 $("body").addClass("loader-open");
              
-                $(".loader__wrap").addClass("view");
-                $('.loader-hidden').removeClass('loader-hidden');
-                localStorage.setItem('lastRun', new Date());
-                setTimeout(function() {
+//                 $(".loader__wrap").addClass("view");
+//                 $('.loader-hidden').removeClass('loader-hidden');
+//                 localStorage.setItem('lastRun', new Date());
+//                 setTimeout(function() {
                 
-                    $(".loader__wrap").addClass("loader-hidden");
-                    setTimeout(function() {
-                        $("body").removeClass("loader-open");
+//                     $(".loader__wrap").addClass("loader-hidden");
+//                     setTimeout(function() {
+//                         $("body").removeClass("loader-open");
                        
-                    },2000)
-                 }, 2500); //1500
-           }
-           else{
-           // $('.loader__wrap').addClass('loader-hidden');
-           console.log('remove');
-            $('.loader__wrap').remove();
-           }
+//                     },2000)
+//                  }, 2500); //1500
+//            }
+//            else{
+//            // $('.loader__wrap').addClass('loader-hidden');
+//            console.log('remove');
+//             $('.loader__wrap').remove();
+            
+//            }
         
      
 
       
             
-        });
+//         });
 
 
 
@@ -39,4 +69,4 @@ $(function() {
         //$(".view").removeClass("loader-hidden");
     //})
 
-});
+//});
