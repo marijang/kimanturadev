6d2938c37c98d1c63cb1047d81dbec8eda4c6e2a
Marijan Greguric <marijan.greguric@in2.hr>
2018-09-05 12:06:40 +0200

    Headroom radi, fix homepage slider

--- wp-content/themes/kimnatura/skin/assets/scripts/headroom.js
+++ wp-content/themes/kimnatura/skin/assets/scripts/headroom.js
@@ -1,20 +1,37 @@
 
+import $ from 'jquery';
+global.$ = global.jQuery = $;
+//
+import Headroom from 'headroom.js';
 
-var Headroom = require("headroom.js");
+//var Headroom = require("headroom.js");
 $(function() {
 // grab an element
 
 var myElement = document.querySelector("#page-navigation");
 // construct an instance of Headroom, passing the element
-var headroom  = new Headroom(myElement);
-
-
+var headroom  = new Headroom(myElement,{
+    // callback when pinned, `this` is headroom object
+    onPin : function() {
+        console.log('OK');
+    },
+    // callback when unpinned, `this` is headroom object
+    onUnpin : function() {console.log('Vidi me kako radim');},
+    // callback when above offset, `this` is headroom object
+    onTop : function() {console.log('Vidi me opet');},
+    // callback when below offset, `this` is headroom object
+    onNotTop : function() {},
+    // callback at bottom of page, `this` is headroom object
+    onBottom : function() {},
+    // callback when moving away from bottom of page, `this` is headroom object
+    onNotBottom : function() {}
+});
 
 // initialise
 
 headroom.init({
     tolerance: 5,
-    offset : 205
+    offset : 205,
 });
 
 });
