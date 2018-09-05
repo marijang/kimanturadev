4b2e32bea2f225c1c24d74fe5e45af6ffbb12247
Marijan Greguric <marijan.greguric@in2.hr>
2018-09-05 13:15:24 +0200

    tabletiranje

--- wp-content/themes/kimnatura/skin/assets/scripts/fonts.js
+++ wp-content/themes/kimnatura/skin/assets/scripts/fonts.js
@@ -2,7 +2,7 @@
 
 WebFont.load({
   google: {
-    families: ['Poppins:300,300i,400,600,700,700i&subset=latin-ext','Material Icons','Merriweather:300,300i,400,600,700,700i&subset=latin-ext'],
+    families: ['Poppins:300,300i,400,600,700,700i:latin-ext','Material Icons','Merriweather:300,300i,400,600,700,700i:latin-ext'],
     //families: ['Poppins:subset=latin-ext','Material Icons','Merriweather:subset=latin-ext'],
   },
 });
