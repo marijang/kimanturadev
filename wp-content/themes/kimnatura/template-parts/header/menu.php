aed80afb7c2755ed764be1ea50a555aa238b9c50
Marijan Greguric <marijan.greguric@in2.hr>
2018-09-06 09:39:39 +0200

    Puno promjena

--- wp-content/themes/kimnatura/template-parts/header/menu.php
+++ wp-content/themes/kimnatura/template-parts/header/menu.php
@@ -5,10 +5,12 @@
  * @package Kimnatura\Template_Parts\Header
  */
 use Kimnatura\Admin\Menu as Menu;
+use Kimnatura\Helpers\General_Helper as Helper;
 $menu             = new Menu\Menu();
 $blog_name        = get_bloginfo( 'name' );
 $blog_description = get_bloginfo( 'description' );
 $header_logo_info = $blog_name . ' - ' . $blog_description;
+$helper = new Helper();
 
 
 $current_user      =  wp_get_current_user();
@@ -18,14 +20,30 @@ if(!$current_user->firstname ){
     $name = $current_user->user_nicename;
     //user_email
 }
-
+$icons_dir =  KIM_IMAGE_URL . 'social-share/';
+$array = array(
+    array(
+        "value" => 'Facebook',
+        "link"  => '#facebook',
+        "image" => 'facebook.svg'
+    ),
+    array(
+        "value" => 'Instagram',
+        "link"  => '#instagram',
+        "image" => 'instagram.svg'
+    ),
+    array(
+        "value" => 'Twitter',
+        "link"  => '#twitter',
+        "image" => 'twitter.svg'
+    )
+);
 ?>
 
 <?php
     echo esc_html( $menu->bem_menu( 'header_main_nav', ' main-navigation--primary main-navigation' ) );
   ?>
-  <div class="header__mobile-user-menu mobile-only">
-      <!--<a href="" class="main-navigation__link header__mobile-lang">Engleski</a>-->
+    <div class="header__mobile-user-login">   
   <?php if(is_user_logged_in()):  ?>
     <a href="/my-account/edit-account" class="main-navigation__link is-logged-in">
             <i class="material-icons">account_circle</i>
@@ -34,7 +52,36 @@ if(!$current_user->firstname ){
     <?php else:  ?>
     <a href="/my-account/edit-account" class="main-navigation__link is-logged-in">
     <i class="material-icons">account_circle</i>
-    <span class="main-navigation__user-info">Log in / Sign up</span>
+    <span class="main-navigation__user-info">Prijava</span>
     </a>
     <?php endif  ?>
+    </div>
+
+  <div class="header__mobile-user-menu mobile-only">
+      <!--<a href="" class="main-navigation__link header__mobile-lang">Engleski</a>-->
+ 
+    <div class="header__mobile-contact">
+     <div class="header__mobile-contact-phone"><a href="tel:+385992317072">+385992317072</a></div>
+     <div class="header__mobile-contact-mail">info@kimnatura.hr</div>
+     <div class="footer__links footer__links--social header__mobile-contact-social">
+        <ul class="social-followus__menu">
+            <?php
+            foreach($array as $item){
+            ?>
+                <li class="social-followus__item">
+                    <a href="<?php echo $item['link']?>" class="social-followus__link">
+                    <?php  get_template_part('skin/public/images/inline/inline',$item['image']) ?>
+                    </a>
+                </li>
+            <?php
+            }
+            ?>
+        </ul>    
+      </div>
+      <div class="header__mobile-contact-copyright"> <?php echo $helper->print_copyright(); ?></div>
+    </div>
+ 
+
+    
+
 </div>
--- wp-content/themes/kimnatura/woocommerce/loop/orderby.php
+++ wp-content/themes/kimnatura/woocommerce/loop/orderby.php
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <form class="woocommerce-ordering shop__products-ordering shop-catalog__ordering" method="get">
-	<select name="orderby" class="orderby select__normal browser-default">
+	<select name="orderby" class="orderby select__normal browser-default1">
 		<?php foreach ( $catalog_orderby_options as $id => $name ) : ?>
 			<option value="<?php echo esc_attr( $id ); ?>" <?php selected( $orderby, $id ); ?>><?php echo esc_html( $name ); ?></option>
 		<?php endforeach; ?>
