975b676fc6d098d90f39fd9d920fdf0b0177f18e
Lovro Filipović <lovro1210@gmail.com>
2018-09-06 15:21:56 +0200

    registracija

--- wp-content/themes/kimnatura/woocommerce/myaccount/form-login.php
+++ wp-content/themes/kimnatura/woocommerce/myaccount/form-login.php
@@ -24,38 +24,77 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <script>
 	jQuery( document ).ready(function() {
-		jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'none');
-		jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'none');
-		jQuery('.woocommerce-privacy-policy-text').css('display', 'none');
+		jQuery('.register').children().css('display', 'none');
+		jQuery('#reg-toggle').parent().css('display', 'block');
+		// jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'none');
+		// jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'none');
+		// jQuery('.woocommerce-privacy-policy-text').css('display', 'none');
 		jQuery('#username').focus();
-		jQuery('#log-toggle').on('click', function(){
-			jQuery('#reg-box').removeClass('is-active');
-			jQuery('#log-box').addClass('is-active');
-			jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'none');
-			jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'none');
-			jQuery('.woocommerce-privacy-policy-text').css('display', 'none');
+	jQuery('#log-toggle').on('click', function(){
+		jQuery('#reg-box').removeClass('is-active');
+		jQuery('#log-box').addClass('is-active');
+		jQuery('.register').children().css('display', 'none');
+		jQuery('#reg-toggle').parent().css('display', 'block');
 	});
 	jQuery('#reg-toggle').on('click', function(){
 			jQuery('#log-box').removeClass('is-active');
 			jQuery('#reg-box').addClass('is-active');
-			jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'inline-block');
-			jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'inline-block');
+			jQuery('.register').children().css('display', 'block');
+			jQuery('#reg-toggle').parent().css('display', 'none');
+			jQuery('.woocommerce-terms-and-conditions').css('display', 'none');
+			// jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'inline-block');
+			// jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'inline-block');
 			//jQuery('.woocommerce-privacy-policy-text').css('display', 'block');
 	});
 	jQuery('#log-toggle-mobile').on('click', function(){
 			jQuery('#reg-box').removeClass('is-active');
 			jQuery('#log-box').addClass('is-active');
-			jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'none');
-			jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'none');
-			jQuery('.woocommerce-privacy-policy-text').css('display', 'none');
+			jQuery('.register').children().css('display', 'none');
+			jQuery('#reg-toggle').parent().css('display', 'block');
 	});
 	jQuery('#reg-toggle-mobile').on('click', function(){
 			jQuery('#log-box').removeClass('is-active');
 			jQuery('#reg-box').addClass('is-active');
-			jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'inline-block');
-			jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'inline-block');
+			jQuery('.register').children().css('display', 'block');
+			jQuery('#reg-toggle').parent().css('display', 'none');
+			jQuery('.woocommerce-terms-and-conditions').css('display', 'none');
 			//jQuery('.woocommerce-privacy-policy-text').css('display', 'block');
 	});
+
+
+	// jQuery( document ).ready(function() {
+	// 	jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'none');
+	// 	jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'none');
+	// 	jQuery('.woocommerce-privacy-policy-text').css('display', 'none');
+	// 	jQuery('#username').focus();
+	// 	jQuery('#log-toggle').on('click', function(){
+	// 		jQuery('#reg-box').removeClass('is-active');
+	// 		jQuery('#log-box').addClass('is-active');
+	// 		jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'none');
+	// 		jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'none');
+	// 		jQuery('.woocommerce-privacy-policy-text').css('display', 'none');
+	// });
+	// jQuery('#reg-toggle').on('click', function(){
+	// 		jQuery('#log-box').removeClass('is-active');
+	// 		jQuery('#reg-box').addClass('is-active');
+	// 		jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'inline-block');
+	// 		jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'inline-block');
+	// 		//jQuery('.woocommerce-privacy-policy-text').css('display', 'block');
+	// });
+	// jQuery('#log-toggle-mobile').on('click', function(){
+	// 		jQuery('#reg-box').removeClass('is-active');
+	// 		jQuery('#log-box').addClass('is-active');
+	// 		jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'none');
+	// 		jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'none');
+	// 		jQuery('.woocommerce-privacy-policy-text').css('display', 'none');
+	// });
+	// jQuery('#reg-toggle-mobile').on('click', function(){
+	// 		jQuery('#log-box').removeClass('is-active');
+	// 		jQuery('#reg-box').addClass('is-active');
+	// 		jQuery('.ct-ultimate-gdpr-consent-field').css('display', 'inline-block');
+	// 		jQuery('.ct-ultimate-gdpr-consent-field-woocommerce').css('display', 'inline-block');
+	// 		//jQuery('.woocommerce-privacy-policy-text').css('display', 'block');
+	// });
 });
 </script>
 
