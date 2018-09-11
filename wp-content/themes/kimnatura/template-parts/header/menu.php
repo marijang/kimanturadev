b573b5aacf45d2b6568205a31ab8633b48920766
Lovro Filipović <lovro1210@gmail.com>
2018-09-11 14:07:01 +0200

    share ikone

--- wp-content/themes/kimnatura/template-parts/header/menu.php
+++ wp-content/themes/kimnatura/template-parts/header/menu.php
@@ -70,7 +70,7 @@ $array = array(
             ?>
                 <li class="social-followus__item">
                     <a href="<?php echo $item['link']?>" class="social-followus__link">
-                    <?php  get_template_part('skin/public/images/inline/inline',$item['image']) ?>
+                    <?php  get_template_part('skin/public/images/inline/inline-'.$item['image']) ?>
                     </a>
                 </li>
             <?php
