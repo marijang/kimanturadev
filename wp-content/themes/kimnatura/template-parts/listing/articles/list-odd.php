d94a94909f6a677c742e8910b27f074663c1df3f
Lovro Filipović <lovro1210@gmail.com>
2018-09-25 14:58:37 +0200

    tablet i mobile novosti

--- wp-content/themes/kimnatura/template-parts/listing/articles/list-odd.php
+++ wp-content/themes/kimnatura/template-parts/listing/articles/list-odd.php
@@ -26,14 +26,23 @@ $image  = $images->get_post_image( 'listing' );
       <a href="<?php echo  esc_url( get_permalink() )?>" rel="bookmark" class="article-list__link"><?php echo __('Saznaj više','kimnatura');?></a>
     </div>
     <div class="article-list__image">
-      <a class="article-list__image-link" href="<?php the_permalink(); ?>">
-        <?php  //if (wp_is_mobile()) {
-            //the_post_thumbnail( 'post_full_width');
-          //} else {
-            the_post_thumbnail( 'blog-list');
-         // }
-        ?>
+    <?php  if (wp_is_mobile()) { ?>
+      <a class="article-list__image-link mobile" href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail( 'post_full_width'); ?>
       </a>
+      <a class="article-list__image-link tablet" href="<?php the_permalink(); ?>">
+        <?php the_post_thumbnail( 'blog-list'); ?>
+      </a>
+    <?php 
+      } else { ?>
+        <a class="article-list__image-link" href="<?php the_permalink(); ?>">
+          <?php
+            the_post_thumbnail( 'blog-list');
+          ?>
+        </a>
+        <?php
+          }
+        ?>
     </div>
   </div>
   <?php require locate_template( 'template-parts/parts/google-rich-snippets.php' ); ?>
