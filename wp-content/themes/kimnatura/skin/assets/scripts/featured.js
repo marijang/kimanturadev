b036334746a1068668582a6e5dbb2a9e872b66b0
Lovro Filipović <lovro1210@gmail.com>
2018-09-25 14:23:36 +0200

    featured img

--- wp-content/themes/kimnatura/skin/assets/scripts/featured.js
+++ wp-content/themes/kimnatura/skin/assets/scripts/featured.js
@@ -2,10 +2,11 @@ import $ from 'jquery';
 global.$ = global.jQuery = $;
 
 $(document).ready(function() {
-    var imageActive = $('.slick-active .featured-link__image');
-    var h = imageActive.height();
-    if (h == 0){
-        h = '250px';
+    var imageActive = $('.slick-list .featured-link__image');
+    var w = imageActive.width();
+    if (w == 0){
+        w = 358;
     }
+    var h = w/1.432;
     $('.slick-list .featured-link__image').css('max-height',h);
 });
