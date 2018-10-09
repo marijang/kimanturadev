6bc5d026c700de2b57bf6b915a833d5af1761b8a
mZabcic <mislav.zabcic@gmail.com>
2018-10-09 14:42:05 +0200

    Popravak checkbox

--- wp-content/themes/kimnatura/template-parts/header/head/loader.php
+++ wp-content/themes/kimnatura/template-parts/header/head/loader.php
@@ -1,4 +1,4 @@
-<div class="loader__wrap view1 loader-hidden1">
+<div class="loader__wrap1 view1 loader-hidden1">
     <div class="loader__bg"></div>
     <div class="loader"><div class="loader-img"></div></div>
 </div>
