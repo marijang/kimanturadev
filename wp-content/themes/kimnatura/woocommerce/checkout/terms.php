dd18bdd93396bca44a81bd094f96f71072a3cf16
mZabcic <mislav.zabcic@gmail.com>
2018-10-15 15:50:47 +0200

    WPML strings

--- wp-content/themes/kimnatura/woocommerce/checkout/terms.php
+++ wp-content/themes/kimnatura/woocommerce/checkout/terms.php
@@ -24,7 +24,7 @@ if ( $terms_page_id > 0 && apply_filters( 'woocommerce_checkout_show_terms', tru
 	<p class="form-row terms wc-terms-and-conditions">
 		<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
 			<input type="checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" name="terms" <?php checked( apply_filters( 'woocommerce_terms_is_checked_default', isset( $_POST['terms'] ) ), true ); ?> id="terms" /> 
-			<span><?php printf( __( 'Pristajem na <a href="/uvjeti-koristenja-internetske-stranice/" target="_blank" class="1woocommerce-terms-and-conditions-link">uvjete korištenja</a>', 'woocommerce' ), "/uvjeti-koristenja-internetske-stranice" ); ?></span> <span class="required">*</span>
+			<span><?php printf( __( 'Pristajem na <a href="/uvjeti-koristenja-internetske-stranice/" target="_blank" class="woocommerce-terms-and-conditions-link">uvjete korištenja</a>', 'woocommerce' ), "/uvjeti-koristenja-internetske-stranice" ); ?></span> <span class="required">*</span>
 		</label>
 		<input type="hidden" name="terms-field" value="1" />
 	</p>
